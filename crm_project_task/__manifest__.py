f49b9d41015114a5c288af507532bcb5e39294ff
MohamedOsman7 <mohamed.osman@glueckkanja.com>
2025-08-05 10:38:08 +0000

    [MIG] crm_project_task: Migration to 18.0

--- crm_project_task/__manifest__.py
+++ crm_project_task/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "CRM Project Task",
     "summary": "Create tasks from lead or opportunity",
-    "version": "17.0.1.0.1",
+    "version": "18.0.1.0.1",
     "development_status": "Alpha",
     "category": "Sales/CRM",
     "website": "https://github.com/OCA/crm",
