79bdf5b13ec72d03cc2dd658b00ef415f7acb093
Ruchir Shukla <ruchir@bizzappdev.com>
2025-10-01 20:17:15 +0530

    [MIG] crm_project_create: Migration to 19.0

--- crm_project_create/__manifest__.py
+++ crm_project_create/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "CRM Project Create",
     "summary": "Allow create projects from lead/opportunity",
-    "version": "18.0.1.0.1",
+    "version": "19.0.1.0.0",
     "category": "Sales/CRM",
     "website": "https://github.com/OCA/crm",
     "author": "Moduon, Odoo Community Association (OCA)",
