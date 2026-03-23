7b07a17e05818501e375b301dbabfbe6dc9f7776
Liam Noonan <ljmnoonan@gmail.com>
2026-03-23 04:38:55 +0000

    [MIG] crm_project_task: Migration to 19.0

--- crm_project_task/__manifest__.py
+++ crm_project_task/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "CRM Project Task",
     "summary": "Create tasks from lead or opportunity",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "development_status": "Alpha",
     "category": "Sales/CRM",
     "website": "https://github.com/OCA/crm",
