ea583703ffff0e5a4e255394caa305c8d6fdc89c
Christopher Rogos <crogos@gmail.com>
2026-04-02 13:49:30 +0000

    [MIG] crm_partner_assign: Migration to 19.0

--- crm_partner_assign/__manifest__.py
+++ crm_partner_assign/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "CRM Partner Assign",
     "summary": "Assign a Partner to an Opportunity/Lead/Partner"
     " to indicate Partnership",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "development_status": "Alpha",
     "category": "Sales/CRM",
     "website": "https://github.com/OCA/crm",
